d842961641c6b9931667b4a8f42a2b0227954cef
Ivan Bogdanov <wexterhack@gmail.com>
2021-04-20 17:33:33 +0300

    Grappelli admin.

--- webapp/settings.py
+++ webapp/settings.py
@@ -33,6 +33,7 @@ ALLOWED_HOSTS = []
 # Application definition
 
 INSTALLED_APPS = [
+    'grappelli',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
@@ -126,3 +127,5 @@ STATIC_URL = '/static/'
 # https://docs.djangoproject.com/en/3.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+GRAPPELLI_ADMIN_TITLE = 'BUC-Crawler'
