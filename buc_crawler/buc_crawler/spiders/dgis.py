437d144327b19a23bb9261a5ca23943f70bdfd3e
horasachy <misha89891@mail.ru>
2021-05-20 17:26:50 +0500

    2gis

--- buc_crawler/buc_crawler/spiders/dgis.py
+++ buc_crawler/buc_crawler/spiders/dgis.py
@@ -1,19 +1,21 @@
 import time
 import logging
-from typing import List
+import json
 
-from selenium.webdriver.common.by import By
+from typing import List, Union, Tuple, AnyStr
 
-from buc_crawler.xpaths import DGisMapPath
 from scrapy import Spider, Selector, Request
-from selenium.webdriver.firefox.webdriver import WebDriver
 from scrapy.loader import ItemLoader
 from scrapy_selenium import SeleniumRequest
-from selenium.webdriver.common.keys import Keys
-from selenium.common.exceptions import NoSuchElementException
 from scrapy_splash import SplashRequest
-from buc_crawler.items import CompanyItem
+from selenium.webdriver.common.keys import Keys
+from selenium.webdriver.firefox.webdriver import WebDriver
+from selenium.webdriver.common.by import By
+from selenium.common.exceptions import NoSuchElementException
 from selenium.webdriver.support import expected_conditions as EC
+
+from buc_crawler.xpaths import DGisMapPath
+from buc_crawler.items import CompanyItem
 
 logger = logging.getLogger(__name__)
 
@@ -23,6 +25,12 @@ xpath = DGisMapPath()
 class DGisSpider(Spider):
     name = '2gis'
 
+    def __init__(self, categories: Union[List, Tuple, AnyStr] = None, cities: Union[List, Tuple, AnyStr] = None,
+                 **kwargs):
+        super(DGisSpider, self).__init__()
+        self.categories = categories.split(',')
+        self.cities = cities.split(',')
+
     allowed_domains = [
         '2gis.ru',
     ]
@@ -30,19 +38,7 @@ class DGisSpider(Spider):
     start_url = [
         'https://2gis.ru',
     ]
-
-    cities = [
-        'Москва', 'Московская область', 'Белгородская область',
-        'Брянская область', 'Владимирская область', 'Воронежская область',
-        'Ивановская область', ' Калужская область', 'Костромская область',
-        'Курская область', 'Липецкая область', 'Орловская область',
-        'Рязанская область', 'Смоленская область', 'Тамбовская область',
-        'Тульская область', 'Ярославская область'
-    ]
-    category_list = [
-        'Металлопрокат', 'Строительная компания', 'Торгово-производственная компания',
-        'Строительные материалы', 'Чёрный металлопрокат', 'Цветной металл'
-    ]
+    cookie_flag = True
     custom_settings = {
         'BOT_NAME': 'Thank you so much!',
         'ROBOTSTXT_OBEY': False,
@@ -68,22 +64,22 @@ class DGisSpider(Spider):
     def start_requests(self):
         for city in self.cities:
             yield SeleniumRequest(
-                    url=self.start_url[0],
-                    callback=self.parse_category_links,
-                    cb_kwargs={'city': city},
-                    wait_time=10,
-                    wait_until=EC.presence_of_element_located((By.XPATH, xpath.search)),
-                    dont_filter=True
-                )
+                url=self.start_url[0],
+                callback=self.parse_category_links,
+                cb_kwargs={'city': city},
+                wait_time=10,
+                wait_until=EC.presence_of_element_located((By.XPATH, xpath.search)),
+                dont_filter=True
+            )
 
     def parse_category_links(self, response, **kwargs):
         driver: WebDriver = response.request.meta['driver']
+        if self.cookie_flag:
+            accept_cookie(driver)
+            self.cookie_flag = False
         city: str = kwargs.get("city")
         urls: List = []
-        accept_cookie(driver)
-        for category in self.category_list:
-            print("_______________")
-            print(len(urls))
+        for category in self.categories:
             search_field = driver.find_element_by_xpath(xpath.search)
             time.sleep(3)
             search_field.send_keys(f'{city} {category}')
@@ -93,6 +89,7 @@ class DGisSpider(Spider):
             fill_and_prepare_urls_of_companies(driver, city, category, urls)
             time.sleep(3)
             search_clear(driver)
+            time.sleep(2)
 
         for url in urls:
             yield SplashRequest(
@@ -117,37 +114,49 @@ def fill_and_prepare_urls_of_companies(driver: WebDriver, city: str, category: s
     while True:
         logger.info(f"City:{city} Category:{category}")
         urls_xpath: List[Selector] = driver.find_elements_by_xpath(xpath.companies_urls)
-        try:
-            next_page: Selector = driver.find_element_by_xpath(xpath.next_page_one)
-        except NoSuchElementException:
-            next_page: Selector = driver.find_element_by_xpath(xpath.next_page_two)
-        driver.execute_script("arguments[0].scrollIntoView();", urls_xpath[-1])
-        time.sleep(1)
+        links_hrefs = [link.get_attribute('href') for link in urls_xpath]
+        if urls_xpath:
+            driver.execute_script("arguments[0].scrollIntoView();", urls_xpath[-1])
+            time.sleep(3)
         if len(urls_xpath) == 12:
-            for url in urls_xpath:
-                urls.append({'href': url.get_attribute("href"), 'city': city, 'category': category})
+            next_page = get_next_page(driver)
+            for url in links_hrefs:
+                urls.append({'href': url, 'city': city, 'category': category})
             next_page.click()
             time.sleep(1)
         else:
-            for url in urls_xpath:
-                urls.append({'href': url.get_attribute("href"), 'city': city, 'category': category})
+            for url in links_hrefs:
+                urls.append({'href': url, 'city': city, 'category': category})
             time.sleep(1)
             break
 
 
 def accept_cookie(driver: WebDriver) -> None:
     try:
-        accept_cookie: Selector = driver.find_element_by_xpath(xpath.accept_cookie)
-        accept_cookie.click()
+        accept_cookie_submit: Selector = driver.find_element_by_xpath(xpath.accept_cookie)
+        accept_cookie_submit.click()
         time.sleep(3)
-    except NoSuchElementException:
-        pass
+    except NoSuchElementException as e:
+        logger.error(e, exc_info=True)
 
 
 def search_clear(driver: WebDriver) -> None:
     try:
-        search_clear: Selector = driver.find_element_by_xpath(xpath.search_clear)
-        search_clear.click()
+        search_clear_btn: Selector = driver.find_element_by_xpath(xpath.search_clear)
+        search_clear_btn.click()
         time.sleep(3)
-    except NoSuchElementException:
-        pass
+    except NoSuchElementException as e:
+        logger.error(e, exc_info=True)
+
+
+def get_next_page(driver: WebDriver, next_page=None) -> Union[Selector, None]:
+    try:
+        next_page: Selector = driver.find_elements_by_xpath(xpath.next_page)
+    except NoSuchElementException as e:
+        logger.error(e, exc_info=True)
+    if len(next_page) > 1:
+        next_page = driver.find_element_by_xpath(f'{xpath.next_page}[2]')
+    elif len(next_page) == 1:
+        next_page = driver.find_element_by_xpath(f'{xpath.next_page}[1]')
+
+    return next_page
